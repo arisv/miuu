d8e432e58eda84c30de0c7b5f69fe6d00604f97a
arisv <tageri7@gmail.com>
2019-08-16 19:48:31 +0300

    debug

--- src/CacheKernel.php
+++ src/CacheKernel.php
@@ -7,5 +7,11 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class CacheKernel extends HttpCache
 {
-
+    protected function getOptions()
+    {
+        return [
+            'debug' => true,
+            'allow_reload' => true
+        ];
+    }
 }
